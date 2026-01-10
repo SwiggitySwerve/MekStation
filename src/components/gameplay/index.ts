f6d2c9569212cd5e99700712906aa7896f3debbf
Wes Rollings <wrollings@gmail.com>
2026-01-09 18:21:46 -0700

    feat: export AmmoCounter component from gameplay index

--- src/components/gameplay/index.ts
+++ src/components/gameplay/index.ts
@@ -2,3 +2,5 @@ export { ArmorPip, ArmorPipGroup } from './ArmorPip';
 export type { ArmorPipProps, ArmorPipGroupProps, PipState } from './ArmorPip';
 export { HeatTracker } from './HeatTracker';
 export type { HeatTrackerProps, HeatScale } from './HeatTracker';
+export { AmmoCounter } from './AmmoCounter';
+export type { AmmoCounterProps } from './AmmoCounter';
