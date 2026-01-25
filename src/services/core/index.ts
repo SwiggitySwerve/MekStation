03d70a72c3196f385e31538b87a58cc311a6d713
Wes Rollings <wrollings@gmail.com>
2026-01-25 01:24:04 -0700

    refactor(services): add ICrudRepository interface for repository standardization

---
+++ src/services/core/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Core Services
+ *
+ * Exports common utilities and interfaces used across all service layers.
+ */
+
+export { createSingleton } from './createSingleton';
+export type { ICrudRepository } from './ICrudRepository';
